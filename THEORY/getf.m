58e4dd8d269efeb6807170c92255cb2e2776bada
tzuohann <tzuohann@gmail.com>
2017-07-07 15:26:48 -0400

    added discretitzationto theory

---
+++ THEORY/getf.m
@@ -0,0 +1,41 @@
+function f_discrete = getf(wStar,phi_db,phi_e,phi_d_fun,aalpha,phi_lim,U_given)
+  globalDeclaration
+  period = 2;
+  E2_discrete = calcExpectedUtil(period,wStar,phi_db,phi_e,phi_d_fun,aalpha,phi_lim);
+  period = 3;
+  E3_discrete = calcExpectedUtil(period,wStar,phi_db,phi_e,phi_d_fun,aalpha,phi_lim);
+  f_discrete = 0;
+  switch whichCommitment
+    case {'perfect'}
+      for ip = 1:length(phi_vec)
+        phi           = phi_vec(ip);
+        if phi < phi_e
+          f_point = (U_given - (1+BETA+BETA^2)*utilFunc(b,ssigma,1))...
+            /(BETA*(E2_discrete + BETA*utilFunc(b,ssigma,1) ...
+            - (1+BETA)*utilFunc(b,ssigma,1)));
+        elseif phi >= phi_e
+          f_point = (U_given - (1+BETA+BETA^2)*utilFunc(b,ssigma,1))...
+            /(BETA*(E2_discrete + BETA*E3_discrete ...
+            - (1+BETA)*utilFunc(b,ssigma,1)));
+        end
+      end
+      f_discrete = f_discrete + f_point;
+      f_discrete = f_discrete/length(phi_vec);
+    case {'limited'}
+      for ip = 1:length(phi_vec)
+        phi           = phi_vec(ip);
+        if phi < phi_lim
+          f_point = (U_given - (1+BETA+BETA^2)*utilFunc(b,ssigma,1))...
+            /(BETA*(E2_discrete + BETA*utilFunc(b,ssigma,1) ...
+            - (1+BETA)*utilFunc(b,ssigma,1)));
+        else if phi >= phi_lim
+            f_point = (U_given - (1+BETA+BETA^2)*utilFunc(b,ssigma,1))...
+              /(BETA*(E2_discrete + BETA*E3_discrete ...
+              - (1+BETA)*utilFunc(b,ssigma,1)));
+          end
+        end
+        f_discrete = f_discrete + f_point;
+      end
+      f_discrete = f_discrete/length(phi_vec);
+  end
+end
